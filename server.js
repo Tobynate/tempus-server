7f41a0beaff8c76eb4c7201106d978d26b36b1c4
Oluwayomi Ilesanmi <oluwayomi.ilesanmi@orangeislandng.com>
2023-09-18 14:05:53 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+
+// start express app
+const app = express()
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
